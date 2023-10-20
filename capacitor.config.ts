c6f293d04e9dee226791999a05ce9f9b4404bd40
Denisa <denibel04@gmail.com>
2023-10-20 08:54:26 +0200

    Angular project

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'conceptos-basicos',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
