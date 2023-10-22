e203df5113725ab4479d29897ae38cb7645cc61c
Denisa <denibel04@gmail.com>
2023-10-22 22:51:45 +0200

    created shared module and authors-info component

---
+++ src/app/core/interface/authors.ts
@@ -0,0 +1,7 @@
+import { Url } from "url";
+
+export interface Authors {
+    name:string,
+    surname:string,
+    github:Url
+}
---
+++ src/app/shared/components/authors-info/authors-info.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-authors-info',
+  templateUrl: './authors-info.component.html',
+  styleUrls: ['./authors-info.component.scss'],
+})
+export class AuthorsInfoComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
