81e15f36c131c71f29bc4c60bc78def169d3ef16
Denisa <denibel04@gmail.com>
2023-10-22 23:36:56 +0200

    fixed shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { IonicModule } from '@ionic/angular';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AuthorsInfoComponent } from './components/authors-info/authors-info.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AuthorsInfoComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    IonicModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  exports: [
+    CommonModule,
+    IonicModule,
+    FormsModule,
+    AuthorsInfoComponent
   ]
 })
 export class SharedModule { }
