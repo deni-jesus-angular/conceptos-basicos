95bac2ee19a16c14094db1823777886660140700
Denisa <denibel04@gmail.com>
2023-10-24 11:14:44 +0200

    added photos to characters

--- src/app/core/services/character.service.ts
+++ src/app/core/services/character.service.ts
@@ -20,9 +20,9 @@ export class CharacterService {
   public getAll():Observable<Characters[]> {
     return new Observable(observer => {
       var _characters = [
-        {id: 1, name:"Naruto", surname:"Uzumaki", source:"Naruto Shippuden", sourceType: "Anime", sourceChapters:582},
-        {id: 2, name:"Geralt", surname:"De Rivia", source:"The Witcher", sourceType: "TV Series", sourceChapters:31},
-        {id: 3, name:"Coco", surname:"", source:"Witch Hat Atelier", sourceType: "Manga", sourceChapters:81},
+        {id: 1, name:"Naruto", surname:"Uzumaki", source:"Naruto Shippuden", sourceType: "Anime", sourceChapters:582, characterPhoto: new URL("https://i.pinimg.com/1200x/ee/61/37/ee61374e60f036d0d605c37b3a7bee8a.jpg")},
+        {id: 2, name:"Geralt", surname:"De Rivia", source:"The Witcher", sourceType: "TV Series", sourceChapters:31, characterPhoto: new URL("https://acortar.link/qjSoZ4")},
+        {id: 3, name:"Coco", surname:"", source:"Witch Hat Atelier", sourceType: "Manga", sourceChapters:81, characterPhoto: new URL("https://ramenparados.com/wp-content/uploads/2021/05/1622290109162-scaled.jpg")},
       ]
       this.id=3;
       observer.next(_characters);
